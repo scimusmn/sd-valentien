b100c0c7590311174caec5a1a685b9ded4b3f570
bryan kennedy <bkennedy@smm.org>
2014-03-17 21:09:06 -0500

    Adding a basic thumbnail gallery
    Using the bindAttr feature to get the thumbnail url form the model to
    build a very basic image gallery.

--- app/scripts/models/painting_model.js
+++ app/scripts/models/painting_model.js
@@ -1,6 +1,7 @@
 /*global Ember*/
 SdValentien.Painting = DS.Model.extend({
-    name: DS.attr('string')
+    name: DS.attr('string'),
+    thumbURL: DS.attr('string')
 });
 
 // probably should be mixed-in...
@@ -13,9 +14,8 @@ SdValentien.Painting.reopen({
   }.property()
 });
 
-// delete below here if you do not want fixtures
 SdValentien.Painting.FIXTURES = [
-    { id: 0, name: 'foo' },
+    { id: 0, name: 'foo' , thumbURL: 'http://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/12-07-12-wikimania-wdc-by-RalfR-010.jpg/500px-12-07-12-wikimania-wdc-by-RalfR-010.jpg'},
     { id: 1, name: 'foo' },
     { id: 2, name: 'foo' },
     { id: 3, name: 'foo' },
